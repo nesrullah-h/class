f7d30f358d94956385bfe5866da2ce32897d1eed
Nesrullah Hacizade <nesrullah.hacizade@gmail.com>
2022-03-11 09:58:14 +0400

    feature class

---
+++ class_task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Class_and__Inheritance.Models;
+
+namespace Class_and__Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.productName = "Love your desease";
+            book1.costPrice = 15;
+            book1.salePrice = 35;
+            book1.saledPrice = 20;
+            book1.authorName = "Valeriy Sinelnikov";
+            book1.pageCount = 92;
+            book1.discountPercent = 5;
+            book1.getInfo();
+            Console.WriteLine($"Discounted price:{book1.GetDiscountedPrice()}");
+        }
+    }
+}
---
+++ class_task/book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_and__Inheritance.Models
+{
+    internal class Book : Product
+    {
+        public string authorName;
+        public int pageCount;
+        public double discountPercent;
+
+        public void getInfo()
+        {
+            Console.WriteLine($"AuthorName:{authorName}\nPageCount:{pageCount}\nDiscountPercent:{discountPercent}%\nproductName:{productName}\nCostPrice:{costPrice}\nSaledPrice: {saledPrice}");
+        }
+
+
+        public double GetDiscountedPrice()
+        {
+            return (salePrice - ((salePrice * discountPercent) / 100));
+        }
+
+    }
+}
